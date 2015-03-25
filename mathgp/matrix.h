5698e402d37480a6536752711ce5ea73d04b50d5
Borislav Stanimirov <b.stanimirov@abv.bg>
2015-03-25 18:45:32 +0200

    fixed the orthogonal projection

--- mathgp/matrix.h
+++ mathgp/matrix.h
@@ -570,7 +570,7 @@ public:
         return matrix4x4t::rows(
             2/width, 0,        0,          0,
             0,       2/height, 0,          0,
-            0,       0,        1/depth,   -1/depth,
+            0,       0,        1/depth,   -near_dist/depth,
             0,       0,        0,          1);
     }
 
