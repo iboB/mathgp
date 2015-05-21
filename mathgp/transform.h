696e04ca9570dfe0f3bf23104ad42d1fb47a6bf2
Borislav Stanimirov <b.stanimirov@abv.bg>
2015-05-21 14:26:04 +0300

    optimized transform_coord

--- mathgp/transform.h
+++ mathgp/transform.h
@@ -39,9 +39,15 @@ _vector_type operator*(const matrixnxnt<_n, _type, _matrix_type>& m, const vecto
 template <typename _type>
 vector3t<_type> transform_coord(const vector3t<_type>& v, const matrix4x4t<_type>& m)
 {
-    vector4t<_type> transform = m * vector4t<_type>::coord(v, 1);
+    vector3t<_type> out;
+    
+    _type w = m(3, 0) * v[0] + m(3, 1) * v[1] + m(3, 2) * v[2] + m(3, 3);
 
-    return transform.homogenous_normalize().xyz();
+    out[0] = (m(0, 0) * v[0] + m(0, 1) * v[1] + m(0, 2) * v[2] + m(0, 3)) / w;
+    out[1] = (m(1, 0) * v[0] + m(1, 1) * v[1] + m(1, 2) * v[2] + m(1, 3)) / w;
+    out[2] = (m(2, 0) * v[0] + m(2, 1) * v[1] + m(2, 2) * v[2] + m(2, 3)) / w;
+
+    return out;
 }
 
 }
